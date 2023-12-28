8f1c268b73c27ee9bce38b24ca14dca5c9ee589f
오진서 <s22030@gsm.hs.kr>
2023-12-28 12:05:08 +0900

    add s3 Path

--- src/main/java/com/moa/rapidus/service/Impl/ImageServiceImpl.java
+++ src/main/java/com/moa/rapidus/service/Impl/ImageServiceImpl.java
@@ -1,5 +1,7 @@
 package com.moa.rapidus.service.Impl;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.moa.rapidus.data.entity.Image;
 import com.moa.rapidus.data.entity.Theme;
 import com.moa.rapidus.data.repository.ImageRepository;
@@ -7,22 +9,24 @@ import com.moa.rapidus.data.repository.ThemeRepository;
 import com.moa.rapidus.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class ImageServiceImpl implements ImageService {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.s3.url}")
+    private String s3Url;
+
+    private final AmazonS3 amazonS3;
 
     @Autowired
     private ImageRepository imageRepository;
@@ -33,17 +37,25 @@ public class ImageServiceImpl implements ImageService {
     @Value("${file.upload-dir}")
     private String uploadDir;
 
+    @Autowired
+    public ImageServiceImpl(AmazonS3 amazonS3) {
+        this.amazonS3 = amazonS3;
+    }
+
     @Override
     public Image saveImage(MultipartFile file, String theme, int type) throws IOException {
         String fileName = UUID.randomUUID().toString() + ".jpeg";
-        String filePath = uploadDir + "/images/" + fileName;
+        String filePath = bucket + "/images/" + fileName;
 
-        Path path = Paths.get(filePath);
-        Files.write(path, file.getBytes());
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.getSize());
+        metadata.setContentType(file.getContentType());
+
+        amazonS3.putObject(bucket, filePath, file.getInputStream(), metadata);
+
 
         Image image = Image.builder()
-                .fileName(fileName)
-                .filePath(filePath)
+                .filePath(s3Url+filePath)
                 .likes(0)
                 .theme(theme)
                 .type(type)
@@ -62,28 +74,12 @@ public class ImageServiceImpl implements ImageService {
     }
 
     @Override
-    public List<Image> getImagesByTheme(String theme) {
-        return imageRepository.findByTheme(theme);
-    }
-
-    @Override
-    public List<Image> getImagesByThemeAndType(String theme, int type) {
-        return imageRepository.findByThemeAndType(theme, type);
-    }
-
-    @Override
-    public Image getImage(Long id) {
-        return imageRepository.findById(id).orElse(null);
-    }
-
-    @Override
     public Image likeImage(Long id) {
         Image image = imageRepository.findById(id).orElse(null);
         if (image != null) {
             int newLikes = image.getLikes() + 1;
             Image updatedImage = Image.builder()
                     .id(image.getId())
-                    .fileName(image.getFileName())
                     .filePath(image.getFilePath())
                     .likes(newLikes)
                     .theme(image.getTheme())
@@ -98,22 +94,6 @@ public class ImageServiceImpl implements ImageService {
     @Override
     public Image getTopImageByType(int type) {
         return imageRepository.findTopByTypeOrderByLikesDesc(type);
-    }
-
-    @Override
-    public Resource loadImageAsResource(String filePath) throws MalformedURLException {
-        Resource resource = new UrlResource("file:" + filePath);
-
-        if (resource.exists() || resource.isReadable()) {
-            return resource;
-        } else {
-            throw new RuntimeException("Could not read the file: " + filePath);
-        }
-    }
-
-
-    public void deleteImage(Long id) {
-        imageRepository.deleteById(id);
     }
 }
 
