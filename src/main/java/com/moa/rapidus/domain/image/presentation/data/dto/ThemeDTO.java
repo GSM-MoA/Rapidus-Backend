feaec2f815c57bb1fd7ddae4334445dfdd6c65ac
오진서 <s22030@gsm.hs.kr>
2023-12-29 14:00:48 +0900

    create ThemeDTO

---
+++ src/main/java/com/moa/rapidus/domain/image/presentation/data/dto/ThemeDTO.java
@@ -0,0 +1,15 @@
+package com.moa.rapidus.domain.image.presentation.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ThemeDTO {
+    private Long id;
+
+    private String enName;
+    private String krName;
+}
