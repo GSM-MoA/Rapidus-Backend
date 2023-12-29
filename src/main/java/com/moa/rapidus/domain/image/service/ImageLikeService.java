f2fddfc312465b6c71dbbc2de6eb9d4e8beacf82
오진서 <s22030@gsm.hs.kr>
2023-12-29 14:02:34 +0900

    create ImageLikeService

---
+++ src/main/java/com/moa/rapidus/domain/image/service/ImageLikeService.java
@@ -0,0 +1,6 @@
+package com.moa.rapidus.domain.image.service;
+
+public interface ImageLikeService {
+
+    void execute(Long id);
+}
---
+++ src/main/java/com/moa/rapidus/domain/image/service/Impl/ImageLikeServiceImpl.java
@@ -0,0 +1,25 @@
+package com.moa.rapidus.domain.image.service.Impl;
+
+import com.moa.rapidus.domain.image.domain.entity.Image;
+import com.moa.rapidus.domain.image.domain.repository.ImageRepository;
+import com.moa.rapidus.domain.image.exception.ImageNotFoundException;
+import com.moa.rapidus.domain.image.service.ImageLikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(rollbackFor = Exception.class)
+public class ImageLikeServiceImpl implements ImageLikeService {
+
+    private final ImageRepository imageRepository;
+
+    @Override
+    public void execute(Long id) {
+        Image image = imageRepository.findById(id)
+                .orElseThrow(ImageNotFoundException::new);
+
+        image.like();
+    }
+}
