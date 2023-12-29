2e01c42663afbcc318e76729977c6a824c5be5c9
오진서 <s22030@gsm.hs.kr>
2023-12-29 14:01:37 +0900

    create ThemeService

---
+++ src/main/java/com/moa/rapidus/domain/image/service/ThemeService.java
@@ -0,0 +1,7 @@
+package com.moa.rapidus.domain.image.service;
+
+import com.moa.rapidus.domain.image.presentation.data.dto.ThemeDTO;
+
+public interface ThemeService {
+    ThemeDTO execute(Long id);
+}
